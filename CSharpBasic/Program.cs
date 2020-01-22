5121efa35113e734c15b3f9f0c551aa60aeb34cf
shuheydev <shuhey.dev@gmail.com>
2020-01-23 07:56:27 +0900

    デリゲートを使って判定部分の処理をメソッドとして引数で渡すようにした.

--- CSharpBasic/Program.cs
+++ CSharpBasic/Program.cs
@@ -4,21 +4,28 @@ namespace CSharpBasic
 {
     class Program
     {
+        public delegate bool Judgement(int value);
+
+        static bool IsEven(int n)
+        {
+            return n % 2==0;
+        }
+
         static void Main(string[] args)
         {
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            var result = Count(numbers,3);
+            var result = Count(numbers,IsEven);
 
             Console.WriteLine(result);
         }
 
-        static int Count(int[] numbers,int num)
+        static int Count(int[] numbers,Judgement judge)
         {
             int count = 0;
             foreach(var n in numbers)
             {
-                if(n==num)
+                if(judge(n)==true)
                 { count++; }
             }
 
